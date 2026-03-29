e4d5cd7e57168e06e3605c3e60226bde2b525b60
Puneet Kandpal <puneet.kandpal@gmail.com>
2026-03-29 11:37:44 +0530

    Remove accidental my-v0-project dependency and fix schema import path casing in Sanity config
    - Remove "my-v0-project": "link:" from package.json dependencies and pnpm-lock.yaml
    - Change schema import path from "./sanity/schemaTypes" to "./sanity/schematypes" (lowercase)
    - Update comment to reference './sanity/schematypes' folder with lowercase path

--- sanity.config.ts
+++ sanity.config.ts
@@ -10,14 +10,14 @@ import { structureTool } from "sanity/structure";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env.public";
-import { schema } from "./sanity/schemaTypes";
+import { schema } from "./sanity/schematypes";
 import { structure } from "./sanity/structure";
 
 export default defineConfig({
   basePath: "/studio",
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  // Add and edit the content schema in the './sanity/schematypes' folder
   schema,
   plugins: [
     structureTool({ structure }),
